8371e12b106d1fb3230d3e8b12342ac96b5a55e6
venki <venki@egovernments.org>
2016-10-13 17:42:39 +0530

    Initial Commit

---
+++ src/main/java/org/egov/eGovCalender/Calender.java
@@ -0,0 +1,13 @@
+package org.egov.eGovCalender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calender {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calender.class, args);
+	}
+
+}
---
+++ src/main/java/org/egov/eGovCalender/HelloController.java
@@ -0,0 +1,14 @@
+package org.egov.eGovCalender;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/calender")
+    public String index() {
+        return "Welcome to calender App!";
+    }
+    
+}
